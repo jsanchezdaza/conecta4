c4b65dc5119f60bf6bd1090273db068f19018410
javisan <j.sanchezdaza89@gmail.com>
2025-08-05 22:55:36 +0200

    fix(lint): export App to satisfy react-refresh rule

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 
-const App = () => <div>Conecta4</div>
+export const App = () => <div>Conecta4</div>
 
 createRoot(document.getElementById('root')!).render(<App />)
