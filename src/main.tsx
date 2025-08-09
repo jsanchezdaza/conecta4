892993f9587efa94686db74ed1a6f3821594b7ea
javisan <j.sanchezdaza89@gmail.com>
2025-08-09 13:50:39 +0200

    chore: remove debug code and keep only critical CSS inline

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { Router } from './router'
 import './styles.css'
 
-console.log('🚀 Connect4 App starting...')
+export const App = () => <Router />
 
-export const App = () => {
-  console.log('📱 App component rendering')
-  return <Router />
-}
-
-console.log('🔧 Creating React root...')
 createRoot(document.getElementById('root')!).render(<App />)
